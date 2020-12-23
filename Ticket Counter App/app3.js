05530c94be2c05ed995d46db9a9616343ddc30dc
unknown <rubayzaf2007@gmail.com>
2020-12-23 15:57:25 +0600

    made a ticket counter app

---
+++ Ticket Counter App/app3.js
@@ -0,0 +1,57 @@
+class TicketCounter {
+    constructor(name) {
+        this.name = name;
+        this.locations = [];
+        this.seats = {};
+        this.prices = {};
+    }
+    addDestination(dest) {
+        this.locations.push(dest.location);
+        this.seats[dest.location] = dest.seats;
+        this.prices[dest.location] = dest.price;
+    }
+    isSeatAvailable(location) {
+        var index = this.locations.indexOf(location);
+        if(index == -1) {
+            return false;
+        }
+        else {
+            var available = this.seats[location];
+            if(available > 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    buyTicket(location, money) {
+        var available = this.isSeatAvailable(location);
+        if(available){
+            var price = this.prices[location];
+            if(money < price){
+                    console.log("Give more money please");
+            }
+            else {
+                this.seats[location] = this.seats[location] - 1;
+                console.log("here is the ticket for", location);
+                var returnAmount = money - price;
+                console.log("you will get back", returnAmount);
+            }
+        }
+    }
+}
+class Destination {
+    constructor(location, seats, price){
+            this.location = location;
+            this.seats = seats;
+            this.price = price;
+    }
+}
+var BusInformation = new TicketCounter("Rubayz Ticket Counter LTD.");
+var khulna = new Destination("khulna", 50, 1200);
+BusInformation.addDestination(khulna);
+
+var rangpur = new Destination("rangpur", 40, 1500);
+BusInformation.addDestination(rangpur);
+console.log(BusInformation);
